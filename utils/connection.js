7c74673b5a03c9ceeb6860b391117a0d351bc9ef
Moojig <moojigc@gmail.com>
2020-05-06 23:57:21 -0400

    Changed var name to MONGODB_URI

--- utils/connection.js
+++ utils/connection.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb"),
-    uri = process.env.MONGO_URI ? process.env.MONGO_URI : require('../private.json').dev.MONGO_URI,
+    uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : require('../private.json').production.MONGODB_URI,
     client = new MongoClient(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
