efb9a29b806d801101b2b640bbe79fe479293cf5
Moojig <moojigc@gmail.com>
2020-05-08 22:07:06 -0400

    I had a typo in a variable name...

--- commands/login.js
+++ commands/login.js
@@ -2,7 +2,7 @@
 async function webLogin(message, player) {
     const { MessageEmbed } = require('discord.js');
     const { channelOrDM } = require('../utils/globalFunctions')(message);
-    const url = process.env.MONGODB_URI ? `https://dnd-inventory-web.herokuapp.com/login/${player_.id}` : `http://localhost:3000/login/${player._id}` 
+    const url = process.env.MONGODB_URI ? `https://dnd-inventory-web.herokuapp.com/login/${player._id}` : `http://localhost:3000/login/${player._id}` 
     let embed = new MessageEmbed()
         .setDescription(`You can [login here](${url}) to manage **${player.name}'s** inventory with a graphical interface.`);
     message.author.send(embed);
