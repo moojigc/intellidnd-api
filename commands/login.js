d26b9189d4b907041ee2ca2c73fe0e07338d6cab
Moojig <moojigc@gmail.com>
2020-05-08 22:05:11 -0400

    Forgot to add new link to hosted URL

--- commands/login.js
+++ commands/login.js
@@ -2,8 +2,9 @@
 async function webLogin(message, player) {
     const { MessageEmbed } = require('discord.js');
     const { channelOrDM } = require('../utils/globalFunctions')(message);
+    const url = process.env.MONGODB_URI ? `https://dnd-inventory-web.herokuapp.com/login/${player_.id}` : `http://localhost:3000/login/${player._id}` 
     let embed = new MessageEmbed()
-        .setDescription(`You can [login here](http://localhost:3000/login/${player._id}) to manage **${player.name}'s** inventory with a graphical interface.`);
+        .setDescription(`You can [login here](${url}) to manage **${player.name}'s** inventory with a graphical interface.`);
     message.author.send(embed);
 }
 
