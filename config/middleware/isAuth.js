7302c699446c9a1e645d69e21fc04b1f89ee0632
Moojig <moojigc@gmail.com>
2020-06-14 01:29:07 -0400

    fixed isAuth

--- config/middleware/isAuth.js
+++ config/middleware/isAuth.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
 	if (req.user) {
 		return next();
-	} else if (req.path !== "/") {
+	} else {
 		req.flash("errorMsg", "You must be logged in to view this resource.");
 		return res.redirect("/users/login");
 	}
