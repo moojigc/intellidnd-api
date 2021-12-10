bc4ddb3e437017dbd560fd6d0c29f6378f21790b
Moojig Battsogt <me@moojig.dev>
2021-12-09 22:54:58 -0500

    derp

--- src/lib/twilio.ts
+++ src/lib/twilio.ts
@@ -3,5 +3,5 @@ import Twilio from 'twilio';
 import * as dotenv from "dotenv";
 dotenv.config()
 
-const twilio = Twilio(data.env.TWILIO_SID, data.env.TWILIO_TOKEN);
+const twilio = Twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 export default twilio;
